a634dc10fdef817c585610bdb5f2697a52eddfb2
Jack Belyeu <jackbelyeu@gmail.com>
2024-04-15 11:56:10 -0500

    lorem readable block with scroll

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,7 +15,7 @@ export const Home = () => {
     <div class={styles.Home}>
       <img prop:src={logo} alt="logo" />
       <h1>Solid + Vite + TypeScript</h1>
-      <button onClick={() => fetchDefinition('fish').then(console.log)} />
+      <button onClick={() => fetchDefinition('fish').then(console.log)}>fetchDefinition Tester Button</button>
       <h3>Hello, {userData()?.name ?? 'guest'}!</h3>
       <p>
         Mouse: {x()} x {y()}
@@ -25,6 +25,28 @@ export const Home = () => {
         <button onClick={() => dec()}>-</button>
         <button onClick={() => inc()}>+</button>
       </h3>
+      <div style={{ height: '10rem', overflow: 'scroll' }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iure nisi, ea quidem quisquam accusantium
+        voluptatum aliquid odit fugiat atque magnam suscipit ratione debitis voluptatibus sed sit ex molestiae eos!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iure nisi, ea quidem quisquam accusantium
+        voluptatum aliquid odit fugiat atque magnam suscipit ratione debitis voluptatibus sed sit ex molestiae eos!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iure nisi, ea quidem quisquam accusantium
+        voluptatum aliquid odit fugiat atque magnam suscipit ratione debitis voluptatibus sed sit ex molestiae eos!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iure nisi, ea quidem quisquam accusantium
+        voluptatum aliquid odit fugiat atque magnam suscipit ratione debitis voluptatibus sed sit ex molestiae eos!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iure nisi, ea quidem quisquam accusantium
+        voluptatum aliquid odit fugiat atque magnam suscipit ratione debitis voluptatibus sed sit ex molestiae eos!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iure nisi, ea quidem quisquam accusantium
+        voluptatum aliquid odit fugiat atque magnam suscipit ratione debitis voluptatibus sed sit ex molestiae eos!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iure nisi, ea quidem quisquam accusantium
+        voluptatum aliquid odit fugiat atque magnam suscipit ratione debitis voluptatibus sed sit ex molestiae eos!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iure nisi, ea quidem quisquam accusantium
+        voluptatum aliquid odit fugiat atque magnam suscipit ratione debitis voluptatibus sed sit ex molestiae eos!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iure nisi, ea quidem quisquam accusantium
+        voluptatum aliquid odit fugiat atque magnam suscipit ratione debitis voluptatibus sed sit ex molestiae eos!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iure nisi, ea quidem quisquam accusantium
+        voluptatum aliquid odit fugiat atque magnam suscipit ratione debitis voluptatibus sed sit ex molestiae eos!
+      </div>
     </div>
   );
 };
