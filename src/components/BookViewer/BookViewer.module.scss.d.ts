64b75ae7063672767552693420cb6b07288f806d
jackbelyeu <jackbelyeu@gmail.com>
2024-04-29 16:13:52 -0500

    css module for bookviewer, scroll options

---
+++ src/components/BookViewer/BookViewer.module.scss.d.ts
@@ -0,0 +1,4 @@
+declare const classNames: {
+  readonly BookViewer: 'BookViewer';
+};
+export = classNames;
--- src/components/BookViewer/index.tsx
+++ src/components/BookViewer/index.tsx
@@ -1,16 +1,20 @@
 import ePub from 'epubjs';
 import prideAndPrejudice from '@/assets/prideandprejudice.epub?url';
+import styles from '@/components/BookViewer/BookViewer.module.scss';
 
 export const BookViewer = () => {
   const book = ePub(prideAndPrejudice);
   const rendition = book.renderTo('viewer', { flow: 'paginated', width: '900', height: '600' });
+  // const rendition = book.renderTo('viewer', { flow: 'paginated', width: '900', height: '600' }); flow: 'scrolled-doc' for scroll mode
   rendition.display();
 
   return (
     <>
-      <div id="viewer" />
-      <button onClick={() => rendition.prev()}>{'<'}</button>
-      <button onClick={() => rendition.next()}>{'>'}</button>
+      <div class={styles.BookViewer}>
+        <div id="viewer" />
+        <button onClick={() => rendition.prev()}>{'<'}</button>
+        <button onClick={() => rendition.next()}>{'>'}</button>
+      </div>
     </>
   );
 };
