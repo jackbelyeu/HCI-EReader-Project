3546a77e8d6f94153084d5d20bcd7f1c26ba5da0
jyothibhavani <jyothibhavani.illa@slu.edu>
2024-05-03 18:39:30 -0500

    updated bookmarks page

--- src/pages/Bookmarks/Bookmarks.module.scss.d.ts
+++ src/pages/Bookmarks/Bookmarks.module.scss.d.ts
@@ -1,4 +1,6 @@
 declare const classNames: {
   readonly Bookmarks: 'Bookmarks';
+  readonly BookContainer: 'BookContainer';
+  readonly Book: 'Book';
 };
 export = classNames;
--- src/pages/Bookmarks/index.tsx
+++ src/pages/Bookmarks/index.tsx
@@ -4,7 +4,23 @@ import styles from '@/pages/Bookmarks/Bookmarks.module.scss';
 export const Bookmarks = () => {
   return (
     <div class={styles.Bookmarks}>
-      <h1> This is Bookmarks page </h1>
+      <h1>Bookmarked Pages</h1>
+      <h2 />
+
+      <div class={styles.BookContainer}>
+        <div class={styles.Book}>
+          <h2>Pride And Prejudice</h2>
+          {/* Add bookmarks for Book 1 here */}
+        </div>
+        <div class={styles.Book}>
+          <h2>1984</h2>
+          {/* Add bookmarks for Book 2 here */}
+        </div>
+        <div class={styles.Book}>
+          <h2>The Great Gatsby</h2>
+          {/* Add bookmarks for Book 3 here */}
+        </div>
+      </div>
     </div>
   );
 };
